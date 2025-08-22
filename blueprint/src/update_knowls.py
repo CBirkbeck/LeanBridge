8fe5a84f9e725655a7b09f3886b086dce9b3ea34
David Roe <roed.math@gmail.com>
2025-08-22 01:23:52 -0400

    Move knowls into folders by category

--- blueprint/src/update_knowls.py
+++ blueprint/src/update_knowls.py
@@ -13,7 +13,7 @@ def clean_content(kwl):
     # For now, we just add label and dependencies
     return f"\\subsection{{{kwl['title']}}}\n\\begin{{definition}}\\label{{{kwl['id']}}}\n\\uses{{{','.join(kwl['links'])}}}\n{kwl['content']}\n\\end{{definition}}\n\n\n"
 
-def update_knowls(path_to_lmfdb=None, cats=["nf", "ec", "mf"], delete_old=False):
+def update_knowls(path_to_lmfdb=None, cats=["nf", "ec", "cmf"], delete_old=False):
     if path_to_lmfdb is None:
         path_to_lmfdb = os.path.expanduser("~/lmfdb")
     sys.path.append(path_to_lmfdb)
@@ -21,15 +21,22 @@ def update_knowls(path_to_lmfdb=None, cats=["nf", "ec", "mf"], delete_old=False)
     knowldir = Path("knowls")
 
     cur_knowls = knowldb.get_all_knowls(fields=['id', 'cat', 'content', 'title', 'links', 'defines'], types=[0])
-    old_knowls = set(path.name for path in knowldir.iterdir())
-    deleted_knowls = old_knowls.difference(set(rec["title"] for rec in cur_knowls))
+    old_knowls = set()
+    for cat in knowldir.iterdir():
+        for path in cat.iterdir():
+            old_knowls.add(path.name)
+    deleted_knowls = old_knowls.difference(set(rec["id"] for rec in cur_knowls))
     if deleted_knowls:
         print("Warning: the following knowls have been deleted in the LMFDB:\n" + ",".join(sorted(deleted_knowls)))
         if delete_old:
             for kid in deleted_knowls:
-                os.remove(knowldir / kid)
+                cat = kid.split(".")[0]
+                os.remove(knowldir / cat / kid)
     for rec in cur_knowls:
-        with open(knowldir / rec["id"], "w") as F:
+        kid = rec["id"]
+        cat = kid.split(".")[0]
+        assert cat == rec["cat"]
+        with open(knowldir / cat / rec["id"], "w") as F:
             _ = F.write(clean_content(rec))
     # Now we trim to only things that are dependencies of knowls in the specified categories
     by_id = {rec["id"]: rec for rec in cur_knowls}
@@ -41,15 +48,15 @@ def update_knowls(path_to_lmfdb=None, cats=["nf", "ec", "mf"], delete_old=False)
                 for link in by_id[kid]["links"]:
                     if link in by_id and link not in keep: # link might be a top or bottom knowl...
                         nextkeep.add(link)
-            keep.update(nextkeep)
-            newkeep = nextkeep
+            keep = keep.union(nextkeep)
+            newkeep = copy(nextkeep)
     by_id = {rec["id"]: rec for rec in cur_knowls if rec["id"] in keep}
 
     # Collect definitions in a file
     with open("definitions.txt", "w") as F:
         by_def = defaultdict(list)
         for kid in sorted(by_id):
-            defines = rec["defines"]
+            defines = by_id[kid]["defines"]
             if defines:
                 _ = F.write(kid + ":\n")
                 for term in defines:
@@ -65,17 +72,17 @@ def update_knowls(path_to_lmfdb=None, cats=["nf", "ec", "mf"], delete_old=False)
     cat_names = {
         "nf": "Number fields",
         "ec": "Elliptic curves",
-        "mf": "Modular forms",
+        "cmf": "Modular forms",
         # Add more categories here
     }
     with open("auto_content.tex", "w") as F:
         # We should impose some kind of reasonable order, but that's for later
-        _ = F.write("\\section{{Background}}\n")
+        _ = F.write("\n\\section{{Background}}\n")
         for kwl in cur_knowls:
             if kwl["cat"] not in cats:
-                _ = F.write(f"\\input{{knowls/{kwl['id']}}}\n")
+                _ = F.write(f"\\input{{knowls/{kwl['cat']}/{kwl['id']}}}\n")
         for cat in cats:
-            _ = F.write(f"\\section{{{cat_names[cat]}}}\n")
+            _ = F.write(f"\n\n\\section{{{cat_names[cat]}}}\n")
             for kwl in cur_knowls:
                 if kwl["cat"] == cat:
-                    _ = F.write(f"\\input{{knowls/{kwl['id']}}}\n")
+                    _ = F.write(f"\\input{{knowls/{kwl['cat']}/{kwl['id']}}}\n")
